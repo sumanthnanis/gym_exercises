cefeed40c71284c16f3853642cdaf75b57c4ca6a
s u m a n t h <badampraveena1981@gmail.com>
2023-08-19 23:59:46 +0530

    complete

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,18 @@
 import React from 'react'
-
+import { Typography, Box, Stack } from '@mui/material';
+import Logo from '../assets/images/Logo-1.png';
 const Footer = () => {
   return (
-    <div>
+    <Box mt="80px" bgcolor="#fff3f4">
+      <Stack gap="40px" alignItems="center" px="40px" pt="24px">
+      <img src={Logo} alt="logo" width="200px" height="40px"/>
+    <Typography variant='h5' pb="40px" mt="20px">
+      Made with irritation by Sumanth
+
+    </Typography>
+      </Stack>
       
-    </div>
+    </Box>
   )
 }
 
