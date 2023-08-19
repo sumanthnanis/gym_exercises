e65bfec59c21af570ac14bedf092677fe9bfbbe5
s u m a n t h <badampraveena1981@gmail.com>
2023-08-19 22:12:48 +0530

    search

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import {Link } from 'react-router-dom'
+import {Stack, Button, Typography} from '@mui/material'  
 
-const ExerciseCard = () => {
+const ExerciseCard = ({exercise}) => {
   return (
-    <div>
-      hi
-    </div>
+    <Link className="exercise-card"to={`/exercise/${exercise.id}`}>
+      <img src={exercise.gifUrl} alt={exercise.name} Loading="lazy"/>
+      <Stack direction="row">
+        <Button sx={{ml:'21px' ,color:'#fff', background:'#ffa9a9',
+      fontSize:'14px',borderRadius:'4px',textTransform:'capitalize'
+      }}>
+        {exercise.bodyPart}
+        </Button>
+        <Button sx={{ml:'21px' ,color:'#fff', background:'#ffcc757',
+      fontSize:'14px',borderRadius:'4px',textTransform:'capitalize'
+      }}>
+        {exercise.target}
+        </Button>
+      </Stack>
+      <Typography ml="21px" color="#000" fontWeight="bold" mt="11p" pb="10px" textTransform="capitalize" fontSize="22px" >
+        {exercise.name}
+      </Typography>
+    </Link>
   )
 }
 
--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,10 +1,48 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
+import {Box,Stack,Typography} from '@mui/material'
+import Pagination from '@mui/material/Pagination'
+import {fetchData,exerciseOptions} from '../utils/fetchData'
+import ExerciseCard from './ExerciseCard'
 
-const Exercises = () => {
+const Exercises = ({exercises, setExercises, bodyPart}) => {
+  const [currentPage,setCurrentPage]=useState(1)
+  const exercisesPerPage=9
+  const indexOfLastExercise=currentPage*exercisesPerPage;
+  const indexOfFirstExercise=indexOfLastExercise-exercisesPerPage;
+  const currentExercises=exercises.slice(indexOfFirstExercise,indexOfLastExercise)
+  const paginate=(e,value)=>{
+    setCurrentPage(value)
+    window.scrollTo({top:1800,behavior:'smooth'})
+
+  }
   return (
-    <div>
-      
-    </div>
+    <Box id="exercises"
+    sx={{mt:{lg: '110px'}}}
+    mt="50px"
+    p="20px"
+    >
+      <Typography variant="h3" mb="46px">
+        Showing results for {bodyPart}
+
+      </Typography>
+      <Stack direction="row" sx={{gap:{lg: '110px',xs:'50px'}}}
+      flexWrap="wrap" justifyContent="center"
+      >
+       {currentExercises.map((exercise, index)=>(
+        <ExerciseCard key={index} exercise={exercise}/>
+       ))} 
+      </Stack>
+      <Stack mt="100px" alignItems="center">
+        {exercises.length>9 && (
+          <Pagination color="standard" shape="rounded" default={1} count={Math.ceil(exercises.length/exercisesPerPage)}
+          page={currentPage}
+          onChange={paginate}
+          size="large"
+          >
+
+          </Pagination>)}
+      </Stack>
+    </Box>
   )
 }
 
